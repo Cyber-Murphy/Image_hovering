e7efe5d644b3bf3b51c1847dc87f21d7de172db1
Gaurav Singh <gv00001singh@gmail.com>
2024-03-06 09:25:57 +0530

    Image-Hovering

---
+++ script.js
@@ -0,0 +1,32 @@
+var elem=document.querySelectorAll('.elem')
+elem.forEach((e)=>{
+   
+    e.addEventListener('mouseenter',()=>{
+      
+        e.childNodes[3].style.opacity=1
+    })
+    e.addEventListener('mouseleave',()=>{
+        e.childNodes[3].style.opacity=0
+        })
+    e.addEventListener('mousemove',(dets)=>{
+        e.childNodes[3].style.left=dets.x+"px"
+        e.childNodes[3].style.top=dets.y+"px"
+        })
+
+})
+
+// var elemImage=document.querySelector('#elem1 img')
+
+// elem1.addEventListener('mousemove',(e)=>{
+//     elemImage.style.left=e.x+'px'
+//     elemImage.style.top=e.y+'px'
+
+// })
+// elem1.addEventListener('mouseenter',(e)=>{
+//     elemImage.style.opacity=1
+  
+// })
+// elem1.addEventListener('mouseleave',(e)=>{
+//     elemImage.style.opacity=0
+  
+// })
